bfc5134ca9db88de89d729eda81662066b388f84
Vietanh2332 <anhndv.114010121012@vtc.edu.vn>
2023-05-23 04:14:48 +0700

    0.0.5

--- Controller/AddCart.php
+++ Controller/AddCart.php
@@ -1,5 +1,14 @@
 <?php
-    session_start();
-    $userID = $_SESSION['user_id'];
-    $productID = $_POST['product_id'];
-    $quantity = $_POST['quantity'];
+require_once '../Model/Cart.php';
+session_start();
+$userID = $_SESSION['user_id'];
+$productID = $_POST['product_id'];
+$quantity = $_POST['quantity'];
+$cart = new Cart();
+$addCart = $cart->AddtoCart($userID, $productID, $quantity);
+if ($addCart) {
+    echo "Sản phẩm đã được thêm vào giỏ hàng.";
+} else {
+    echo "Có lỗi xảy ra khi thêm sản phẩm vào giỏ hàng.";
+}
+?>
